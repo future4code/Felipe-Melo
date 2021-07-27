0ec62b53f66c389e572fe84e769812bcb418fb4c
Your Name <you@example.com>
2021-07-27 16:52:06 -0300

    caminhando

---
+++ aula-assincronas/src/App.js
@@ -0,0 +1,29 @@
+
+import React from 'react'
+import './App.css';
+import cadastro from 'cadastro.js';
+
+
+
+
+
+
+
+
+
+
+
+
+function App() {
+  return (
+    <div>
+     <h1>Labefy</h1>
+    <cadastro/>
+    
+    </div>
+
+
+  );
+}
+
+export default App;
