3fc29ea9dedeaed3fdd45309910d9dcfbfbe89e6
Your Name <you@example.com>
2021-08-11 13:49:12 -0300

    finalizado, Amem Senhor \0/

--- semana9/poke-api/src/App.js
+++ semana9/poke-api/src/App.js
@@ -4,61 +4,35 @@ import axios from "axios";
 import PokeCard from "./components/PokeCard";
 
 const  App = () => {
-    const [pokeList, setPokeList] = useState (0)
-    const [PokeName, setPokeName] = useState (0)
-
-  
+    const [pokeList, setPokeList] = useState ([])
+    const [pokeName, setPokeName] = useState ("")
 
     useEffect(() =>{
       axios
       .get("https://pokeapi.co/api/v2/pokemon/?limit=151")
       .then(response => {
-        // função que está setando no estado os 151 pokemons
-        setState({ setPokeList: response.data.results });
+     
+       setPokeList(response.data.results)
       })
       .catch(err => {
         console.log(err);
       });
-  })
-}
+    },[])
+  
  
 
   const changePokeName = (event) => {
-    setState({ setPokeName: event.target.value });
-  
-  
-
-
-
-    
-/* 
-  // método que roda após a montagem do componente
-  componentDidMount = () => {
-    // função axios que está batendo na API e buscando 151 pokemons
-    axios
-      .get("https://pokeapi.co/api/v2/pokemon/?limit=151")
-      .then(response => {
-        // função que está setando no estado os 151 pokemons
-        this.setState({ pokeList: response.data.results });
-      })
-      .catch(err => {
-        console.log(err);
-      });
-  };
-
-  changePokeName = event => {
-    this.setState({ pokeName: event.target.value });
-  }; */
-
+    setPokeName(event.target.value)
+  }
   
     return (
       <div className="App">
-        {/* evento onChange chama função toda vez que o usuário 
-        escolhe um novo pokemon no dropdown */}
+    
         <select onChange={changePokeName}>
+          
           <option value={""}>Nenhum</option>
-          {/* renderizando a lista de pokemons como opções do select */}
-          {setPokeList.map(pokemon => {
+      
+          {pokeList.map(pokemon => {
             return (
               <option key={pokemon.name} value={pokemon.name}>
                 {pokemon.name}
@@ -66,12 +40,11 @@ const  App = () => {
             );
           })}
         </select>
-        {/* expressão booleana que renderiza o componente PokeCard,
-        caso o valor de pokeName, no estado, seja true */}
-        {setPokeName && <PokeCard pokemon={setPokeName} />}
+       
+        {pokeName && <PokeCard pokemon={pokeName} />}
       </div>
     );
-  }
+      }
 
 
 export default App;
--- semana9/poke-api/src/components/PokeCard/index.js
+++ semana9/poke-api/src/components/PokeCard/index.js
@@ -1,67 +1,34 @@
 import React, {useState, useEffect}from "react";
 import axios from "axios";
 
-const PokeCard =() => {
-  const [PokeCard,  setPokeCard] = useState (0)
-  const [pokeName, setPokeName] = useState (0)
-  const [pokemon, setPokemon] = useState (0)
-  
-  /* state = {
-    // valor do estado que guarda infos e foto do pokemon
-    pokemon: {} */
-  
+const PokeCard =(props) => {
 
-  // método que roda após a montagem do componente
- /*  componentDidMount() {
-   pegaPokemon(props.pokemon); */
+ 
+  const [pokemon, setPokemon] = useState ({})
+
   useEffect(()=>{
-    if (pokemon !== setPokemon) {
-      pegaPokemon(setPokemon);
-    }
-  
-  
-  
-  
+    pegaPokemon(props.pokemon)
+  },[props.pokemon])
 
-  /* // método que roda após a atualização do componente.
-  // Um dos casos de atualização do componente é a
-  // mudança da props que está sendo passado pra ele
-  componentDidUpdate(prevProps) {
-    // aqui, é feita uma verificação da props anterior com a props atual.
-    // Caso a props anterior seja diferente da props atual,
-    // a função pegaPokemon é chamada.
-    if (prevProps.pokemon !== this.props.pokemon) {
-      this.pegaPokemon(this.props.pokemon);
-    }
-  } */
-
-  // função que bate na poke API com um nome específico de pokemon
-  // Isso permite que consigamos pegar as infos dos pokemons.
-  // Nos métodos de ciclo de vida, ela é chamada passando como
-  // parâmetro o nome de pokemon que está chegando como props.
   const pegaPokemon = (pokeName) => {
     axios
-      .get(`https://pokeapi.co/api/v2/pokemon/${setPokeName}`)
+      .get(`https://pokeapi.co/api/v2/pokemon/${pokeName}`)
       .then(response => {
         // guarda as infos do pokemon no estado
-        setState({ setPokemon: response.data });
+        setPokemon(response.data)
       })
       .catch(err => {
         console.log(err);
       });
   };
-})
 
- /* 
-    const pokemon = state.pokemon;
- */
     return (
       <div>
-        <p>{setPokemon.name}</p>
-        <p>{setPokemon.weight} Kg</p>
-        {setPokemon.types && <p>{setPokemon.types[0].type.name}</p>}
-        {setPokemon.sprites && (
-          <img src={setPokemon.sprites.front_default} alt={setPokemon.name} />
+        <p>{pokemon.name}</p>
+        <p>{pokemon.weight} Kg</p>
+        {pokemon.types && <p>{pokemon.types[0].type.name}</p>}
+        {pokemon.sprites && (
+          <img src={pokemon.sprites.front_default} alt={pokemon.name} />
         )}
       </div>
     );
