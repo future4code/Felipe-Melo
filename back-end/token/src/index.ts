925e8542be81d4c6e047a37ee55792f7e6def18a
Your Name <you@example.com>
2021-10-19 16:02:10 -0300

    inicio

--- back-end/token/src/index.ts
+++ back-end/token/src/index.ts
@@ -2,7 +2,26 @@ import app from "./app"
 import editUser from './endpoints/editUser'
 import createUser from './endpoints/createUser'
 import { login } from "./endpoints/login"
+import * as bcrypt from "bcryptjs";
+
+
+
+
+
 
 app.post('/users/signup', createUser)
 app.post("/users/login", login)
 app.put('/users', editUser)
+
+
+const hash = async(s:string): Promise<string> =>{
+const rounds = Number(process.env.BCRYPT_COST)
+const salt = await bcrypt.genSalt(rounds)
+const result = await bcrypt.hash(s, salt)
+console.log("encrypted message :", result)
+return result
+}
+
+const compare = async(s: string, hash: string): Promise<boolean>=>{
+    return bcrypt.compare(s,hash)
+}
