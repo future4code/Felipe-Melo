ebdfa1daec04ac738122a330baa33e9240e956e1
Felipevieira29 <felipevieira21@hotmail.com>
2021-11-19 17:15:28 -0300

    inicio de requisicoes

--- rodada-cases/loteria/loteria/src/pages/Home.js
+++ rodada-cases/loteria/loteria/src/pages/Home.js
@@ -1,23 +1,48 @@
 import React from 'react'
-import { Container } from './styled'
-import Numbers from './Numbers'
-import { Sort } from './styled'
 import axios from 'axios'
+import { useState, useEffect } from 'react'
+
 
 
 const Home = () => {
-    
+
+    const [loterias, setLoterias] = useState([])
+    const [megasena,setMegasena]= useState([])
 
 
+       
+    const getLoterias =()=>{
+            
+            axios.get('https://brainn-api-loterias.herokuapp.com/api/v1/Loterias')
+              
+            .then((res)=> {               
+                console.log(res.data.list);
+              })
+              .catch( (error)=> {               
+                console.log(error.response.data);
+              })
+            {
+        }
+        
+        useEffect(() => {
+          getLoterias()
+          return () => {
+           
+          }
+        }, [])
+  
 
     return (
         <div>
        
         <h1>Mega Sena</h1>
-     
+     <button onclick={getLoterias}>Sortear</button>
+
            
         </div>
+
     )
 }
+    
 
 export default Home
